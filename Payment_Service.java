2799a75f85106488cf24774f9806907e6e85a0d6
bawithahk <bawi.thang@mavs.uta.edu>
2025-09-27 17:05:52 -0500

    VP code from Storefront Class Diagram

---
+++ Payment_Service.java
@@ -0,0 +1,26 @@
+public class Payment_Service {
+
+	/**
+	 * 
+	 * @param payment
+	 */
+	public void initiatePayment(Payment payment) {
+		// TODO - implement Payment_Service.initiatePayment
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param payment
+	 */
+	public boolean validatePayment(Payment payment) {
+		// TODO - implement Payment_Service.validatePayment
+		throw new UnsupportedOperationException();
+	}
+
+	public void connectPaymentGateway() {
+		// TODO - implement Payment_Service.connectPaymentGateway
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ Product_Service.java
@@ -0,0 +1,36 @@
+public class Product_Service {
+
+	public List<Product> getProducts() {
+		// TODO - implement Product_Service.getProducts
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param product
+	 */
+	public void addProduct(Product product) {
+		// TODO - implement Product_Service.addProduct
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param productID
+	 * @param updatedProduct
+	 */
+	public void updateProduct(int productID, Product updatedProduct) {
+		// TODO - implement Product_Service.updateProduct
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param productID
+	 */
+	public void deleteProduct(int productID) {
+		// TODO - implement Product_Service.deleteProduct
+		throw new UnsupportedOperationException();
+	}
+
+}
